bdf63150025607f4f3147478016e2fc5c7858e1b
keelann95 <sephen.mwaniki@student.moringaschool.com>
2026-01-26 14:42:01 +0300

    reads the file by first determining the size so as to allocate enough buffer, allocates the required buffer, chooses the location where buffer allocations starts from, sets how many bytes to read, chooses position where the reading file starts from and finally reads the file from start to end

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const fs =  require('fs/promises');
-
-(async ()=>{
-    const watcher = fs.watch("./")
+const fs = require('fs/promises');
+(async()=>{
+    //watches the file
+    const watcher = fs.watch('./command.txt')
+     //read the file
+    const commandFileHandler =await fs.open("./command.txt", 'r')
 
     for await (const event of watcher){
-        console.log(event);
-        
+
+        if(event.eventType ==="change"){
+            //the file was changed
+            
+            //we want to read the content
+
+            //first we get the size in order to allocate enough buffer
+            const contSize = (await commandFileHandler.stat()).size
+            
+            //allocate the buffer to required size
+            const buff = Buffer.alloc(contSize)
+
+            //choose location where the buffer allocation starts from
+            const offset = 0
+
+            //how many bytes to read
+            const length = buff.byteLength
+
+            //the position that we start reading file from
+            const position = 0
+
+             //we always want to read the whole content from start to end
+             const content = await commandFileHandler.read(buff,offset,length,position)
+             console.log(content);
+             
+            
+        }
     }
 })()
