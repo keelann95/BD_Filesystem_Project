9ac2778a23d8883e41005832bc2c80b3359d2af6
keelann95 <sephen.mwaniki@student.moringaschool.com>
2026-01-27 12:36:00 +0300

    successfully manages to write a code that reads my command container and executes the command by creating a file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const createFile = async (path) => {
+    try {
+      const existingPath = await fs.open(path, "r");
+      await existingPath.close();
+      console.log(`The file ${path} already exists`);
+      return;
+    } catch (error) {
+      const createFileHandle = await fs.open(path, "w");
+      console.log("A new file has been successfully created.");
+      createFileHandle.close();
+    }
+  };
+
+  //commands
+  const CREATE_FILE = "create a file";
+
   //read the file
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -21,17 +37,16 @@ const fs = require("fs/promises");
     const position = 0;
 
     //we always want to read the whole content from start to end
-    await commandFileHandler.read(
-      buff,
-      offset,
-      length,
-      position,
-    );
+    await commandFileHandler.read(buff, offset, length, position);
 
-     
+    const command = buff.toString("utf-8");
+    console.log(command);
 
-
-    console.log(buff.toString("utf-8"));
+    //create a file <path>
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
+      createFile(filePath);
+    }
   });
 
   //watches the file
