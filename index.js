d186d86e6dcd4b12db0a1530dbee54a11f9d6f23
keelann95 <sephen.mwaniki@student.moringaschool.com>
2026-01-27 11:45:27 +0300

    decodes the buffers into meaningful text using toString method

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const fs = require('fs/promises');
-(async()=>{
-    //watches the file
-    const watcher = fs.watch('./command.txt')
-     //read the file
-    const commandFileHandler =await fs.open("./command.txt", 'r')
+const fs = require("fs/promises");
 
-    for await (const event of watcher){
+(async () => {
+  //read the file
+  const commandFileHandler = await fs.open("./command.txt", "r");
 
-        if(event.eventType ==="change"){
-            //the file was changed
-            
-            //we want to read the content
+  commandFileHandler.on("change", async () => {
+    //first we get the size in order to allocate enough buffer
+    const contSize = (await commandFileHandler.stat()).size;
 
-            //first we get the size in order to allocate enough buffer
-            const contSize = (await commandFileHandler.stat()).size
-            
-            //allocate the buffer to required size
-            const buff = Buffer.alloc(contSize)
+    //allocate the buffer to required size
+    const buff = Buffer.alloc(contSize);
 
-            //choose location where the buffer allocation starts from
-            const offset = 0
+    //choose location where the buffer allocation starts from
+    const offset = 0;
 
-            //how many bytes to read
-            const length = buff.byteLength
+    //how many bytes to read
+    const length = buff.byteLength;
 
-            //the position that we start reading file from
-            const position = 0
+    //the position that we start reading file from
+    const position = 0;
 
-             //we always want to read the whole content from start to end
-             const content = await commandFileHandler.read(buff,offset,length,position)
-             console.log(content);
-             
-            
-        }
+    //we always want to read the whole content from start to end
+    await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position,
+    );
+
+     
+
+
+    console.log(buff.toString("utf-8"));
+  });
+
+  //watches the file
+  const watcher = fs.watch("./command.txt");
+
+  for await (const event of watcher) {
+    if (event.eventType === "change") {
+      commandFileHandler.emit("change");
     }
-})()
+  }
+})();
