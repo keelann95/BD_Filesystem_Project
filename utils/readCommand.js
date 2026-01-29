2762c851afa154e08e441dab85fb688f53bbb702
keelann95 <sephen.mwaniki@student.moringaschool.com>
2026-01-29 13:00:24 +0300

    modernizes read function

--- utils/readCommand.js
+++ utils/readCommand.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const fsWatch = require('fs')
 require("dotenv").config({
   path: require("path").resolve(__dirname, "../.env"),
 });
@@ -7,33 +8,38 @@ require("dotenv").config({
 const commandPath = path.resolve(__dirname, "..", process.env.COMMAND_FILE);
 (async () => {
   try {
-    //open the file
-    const commandFileContent = await fs.readFile(commandPath, "utf8");
-
-    console.log(commandFileContent);
-
-    return commandFileContent.trim();
-
     //parser
 
-    const parseCommand = async (command) =>{
+    const parseCommand =  (command) =>{
 
         if(!command) return null
 
-        const [action, ...rest] = await command.split(/\s+/)
+        const [action, ...rest] = command.split(/\s+/)
         return{
             action, args: rest.join(" ")
         }
     }
 
-
     //watcher
 
-    
+    let timeout;
+
+    fsWatch.watch(commandPath, async() =>{
+        clearTimeout(timeout)
+        timeout = setTimeout(async()=>{
+            const commandFileCont = await fs.readFile(commandPath, 'utf8')
+            console.log("watching:", commandFileCont);
+
+            const parsed = parseCommand(commandFileCont.trim())
+            console.log(parsed);
+            
+            
+        },100)
+    })
 
 
   } catch (error) {
-    console.error("Failed to read command file:", err.message);
+    console.error("Failed to read command file:", error.message);
     return null;
   }
 })();
